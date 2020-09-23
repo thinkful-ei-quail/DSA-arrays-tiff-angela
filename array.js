92a5fc2da74a9d30a6d0629ea56002c2bcb806af
Tiff Joy <tiffanysummerford@gmail.com>
2020-09-23 14:59:23 -0400

    set up?

--- array.js
+++ array.js
@@ -24,17 +24,40 @@ class Array {
         memory.free(oldPtr)     // adds ptr to free space
         this._capacity = size   // new size of arr
     }
+    get (index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        return memory.get(this.ptr + index);
+    }
+    }
     pop () {
-
+        if (this.length == 0) {
+            throw new Error('Index error');
+        }
+        const value = memory.get(this.ptr + this.length - 1);
+        this.length--;
+        return value;
     }
-    get () {
+    insert(index, value) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
 
-    }
-    insert () {
+        if (this.length >= this._capacity) {
+            this._resize((this.length + 1) * Array.SIZE_RATIO);
+        }
 
+        memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+        memory.set(this.ptr + index, value);
+        this.length++;
     }
-    remove () {
-        
+    remove (index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error('Index error');
+        }
+        memory.copy(this.ptr + index, this.ptr + index + 1, this.length - index - 1);
+        this.length--;
     }
 }
 Array.SIZE_RATIO = 3
